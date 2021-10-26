54c316803d728f2090c74e715c6c3816d13430c0
Giuseppe0197 <faro.g@hotmail.it>
2021-10-26 16:03:52 +0200

    primo esercizio

--- mail/js/myscript.js
+++ mail/js/myscript.js
@@ -9,22 +9,27 @@ let verificaMail = document.getElementById("messaggio");
 let email = prompt("inserisci la mail");
 console.log(email);
 
+let found = false;
+
 /* creiamo un'array */
 
-const mailList = ["gino@hotmail.com", "gino2@hotmail.com"];
+const mailList = ["gino@hotmail.com", "gino2@hotmail.com", "gino3@hotmail.com", "gino4@hotmail.com", "gino5@hotmail.com", "gino6@hotmail.com"];
 console.log(mailList);
 
 /* prova prompt */
 
-    if (email == mailList) {
-
-        verificaMail.innerHTML = `salve ${email}`;
-    
-    } else if (email != mailList){
-    
-        verificaMail.innerHTML = `Mi dispiace ma la sua mail non si trova nella lista`;
-    
+for (i = 0; i < mailList.length; i++){
+    if (email == mailList[i]) {
+        found = true;
     }
+}
+
+if (found == true) {
+    verificaMail.innerHTML = `Salve ${email}`;
+} else {
+    verificaMail.innerHTML = 'Siamo spiacenti ma la sua mail non si trova nella lista.';
+}
+
 
 /* andiamo a dare la funzione di click al bottone */
 
@@ -32,14 +37,16 @@ console.log(mailList);
 
     function(){
 
-        if (emailInserita === mailList) {
-
-            controllo.innerHtml = `Benvenuto nella mail list`;
-
-        } else if (emailInserita !== mailList){
-
-            controllo.innerHtml = `Mi dispiace ma la sua mail non si trova nella lista`;
-
+        for (i = 0; i < mailList.length; i++){
+            if (emailInserita == mailList[i]) {
+                found = true;
+            }
+        }
+        
+        if (found == true) {
+            verificaMail.innerHTML = `Salve ${emailInserita}`;
+        } else {
+            verificaMail.innerHTML = 'Siamo spiacenti ma la sua mail non si trova nella lista.';
         }
 
     }
